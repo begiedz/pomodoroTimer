a9a22b64f55e14d6664d1e42289df62657a963ac
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2023-04-26 15:12:39 +0200

    add break

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ const timer = document.querySelector('.timer')
 const button = document.querySelector('button')
 const minutesSpan = document.querySelector('.minutes')
 const secondsSpan = document.querySelector('.seconds')
+const state = document.querySelector(".state")
 
-let minutes = 25
-let seconds = 0
+let minutes = 0
+let seconds = 5
 intervalRunning = false;
-workTime = false;
+let workTime;
 minutesSpan.textContent = String(minutes).padStart(2, 0)
 secondsSpan.textContent = String(seconds).padStart(2, 0)
 button.textContent = 'Play'
@@ -18,17 +19,34 @@ const StartPomodoro = () => {
     if (!intervalRunning) {
         button.textContent = 'Pause'
         interval = setInterval(pomodoroInterval, 1000)
+
+        workTime = true
         function pomodoroInterval() {
 
             seconds--
 
-            if (seconds <= 0) {
+            if (seconds == 0 && minutes == 0) {
+                workTime = !workTime
+                console.log(workTime);
+                if (!workTime) {
+                    minutes = 15
+                    state.textContent = 'Break'
+                    console.log(workTime);
+                }
+            }
+
+            if (seconds == 0) {
                 minutes--
                 seconds = 59
+
             }
+
+
 
             minutesSpan.textContent = String(minutes).padStart(2, 0)
             secondsSpan.textContent = String(seconds).padStart(2, 0)
+
+
 
         }
         intervalRunning = !intervalRunning
